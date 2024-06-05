b329a878beacec23435792ee23e3c23d4e9e1a2b
ghazraoui <100152714+ghazraoui@users.noreply.github.com>
2024-06-05 13:00:29 +0200

    Tester2

--- Tester.py
+++ Tester.py
@@ -4,7 +4,6 @@ import numpy as np
 from datafilereader import DataFileReader
 #from sklearn.externals import joblib
 
-
 # Path to the data file
 file_path = "./data/Tests/coin_data.h5" 
 
@@ -12,32 +11,32 @@ file_path = "./data/Tests/coin_data.h5"
 featureListR = [17,20,21,22,26,28,31,32,39,42,44,71]
 featureListL = [4,5,6,7,8,9,12,61]
 
-
 # Load the pre-trained SVM model
 # model = joblib.load('svm_model.pkl')
 # scaler = joblib.load('scaler.pkl')
 
-#Main loop to continuously check the file for changes
+# Main function
 def main(file_path):
 
+    #SETUP
     Calibrated = False
     reader = DataFileReader(file_path)
 
     # CALIBRATION
     print("Please Start Calibration")
     while not Calibrated:
-        
+    
         if reader.has_file_changed():
             f, Z, is_reference = reader.get_last_mesurement()
             R_cal = np.real(Z)
             L_cal = np.imag(Z)/(2*np.pi*f)
             Calibrated = True
             print("Calibration done")
-            return R_cal, L_cal
+            return R_cal, L_cal #return the calibration values
 
         time.sleep(5)  # Give user time to calibrate
         
-    # Test
+    # TESTING
     print("Please take the measurement for the coin to be tested")
     while  Calibrated:
         if reader.has_file_changed():
@@ -50,12 +49,16 @@ def main(file_path):
             #extract the needed features
             R = R[featureListR]
             L = L[featureListL]
-            return R, L
+            #concatenate the features
+            # X = np.concatenate((R,L),axis=0)
+            # X = scaler.transform(X)
+            # Y = model.predict(X)
+            # print("Coin is of type: ", Y)
+            return 
 
         time.sleep(5)  # Give user time to mesure the coin to be tested
         
     
-
 main(file_path)
 
 # if __name__ == "__main__":
