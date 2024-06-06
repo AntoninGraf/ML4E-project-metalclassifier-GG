326c32e38d61d649b44d8e82d82c09662c5abc88
AntoninGraf <79448572+AntoninGraf@users.noreply.github.com>
2024-06-06 15:09:24 +0000

    add annomaly detection

--- createSets.py
+++ createSets.py
@@ -50,40 +50,6 @@ for j in range(len(folders)):
             X.append(np.concatenate((R[n,:],L[n,:],p[n,:]),axis=0))
             #X.append(d) ,p[n,:]
             Y.append(i)
-# if enForeign:
-#     # add unknown coins on the validation set and on the training set
-#     folder = "./data/foreign/"
-
-#     labels_name = ["1_EUR","1_LST","1ct_EUR","2ct_EUR","2ct_LST","5ct_EUR", "5ct_LST_1991", "5ct_LST_2015", "10ct_EUR", "10ct_LST", "20ct_EUR", "20ct_LST","50ct_LST"]
-
-#     featureListR = [17,20,21,22,26,28,31,32,39,42,44,71]
-#     featureListL = [4,5,6,7,8,9,12,61]
-
-#     dataset = DataFileReader(folder+labels_name[1]+".h5")
-#     f,_ = dataset.get_all_mesurements()
-
-
-#     for i in range(1, len(labels_name)):
-#         dataset = DataFileReader(folder+labels_name[i]+".h5")
-
-#         #get all measured Z for this coin
-#         _,Z = dataset.get_all_mesurements()
-#         N = len(Z) #number of measurements for this coin
-#         R = np.real(Z)
-#         L = np.imag(Z)/(2*np.pi*f)
-
-#         # substract all the data by the calibration
-#         R = R[1:,:]-R[0,:]
-#         L = L[1:,:]-L[0,:]
-
-#         #extract the needed features
-#         R = R[:,featureListR]
-#         L = L[:,featureListL]
-
-#         #concatenate the features
-#         for n in range(N-1):
-#             X.append(np.concatenate((R[n,:],L[n,:]),axis=0))
-#             Y.append(0)
             
 print("number of data :",len(Y))
 print("number of features :",len(X[0]))
@@ -110,4 +76,55 @@ with open("dataset/testingset.pkl","wb+") as f:
     pickle.dump((X[Ntrain+Nvalid:],Y[Ntrain+Nvalid:]), f)
 
 
+
+#create a validation set with foreign coins
+X_foreign = []
+Y_foreign = []
+
+
+folder = "./data/foreign/"
+
+labels_name = ["1_EUR","1_LST","1ct_EUR","2ct_EUR","2ct_LST","5ct_EUR", "5ct_LST_1991", "5ct_LST_2015", "10ct_EUR", "10ct_LST", "20ct_EUR", "20ct_LST","50ct_LST"]
+
+featureListR = [17,20,21,22,26,28,31,32,39,42,44,71]
+featureListL = [4,5,6,7,8,9,10,12,61]
+
+dataset = DataFileReader(folder+labels_name[1]+".h5")
+f,_ = dataset.get_all_mesurements()
+
+
+for i in range(1, len(labels_name)):
+    dataset = DataFileReader(folder+labels_name[i]+".h5")
+
+    #get all measured Z for this coin
+    _,Z = dataset.get_all_mesurements()
+    N = len(Z) #number of measurements for this coin
+    R = np.real(Z)
+    L = np.imag(Z)/(2*np.pi*f)
+
+    # substract all the data by the calibration
+    R = R[1:,:]-R[0,:]
+    L = L[1:,:]-L[0,:]
+
+    #extract the needed features
+    R = R[:,featureListR]
+    L = L[:,featureListL]
+    p = R[:,0:9]/L
+    #concatenate the features
+    for n in range(N-1):
+        X_foreign.append(np.concatenate((R[n,:],L[n,:],p[n,:]),axis=0))
+        Y_foreign.append(-1)
+
+X_test_foreign = np.concatenate((X_foreign[:25],X[:25]),axis=0)
+Y_test_foreign = np.concatenate((Y_foreign[:25],np.ones(25)),axis=0)
+
+
+#shuffle the data
+X_test_foreign,Y_test_foreign = shuffle(X_test_foreign,Y_test_foreign,random_state=42)
+with open("dataset/testset_foreign.pkl","wb+") as f:
+    pickle.dump((X_test_foreign,Y_test_foreign), f)
+
+print("test set generated with foreign coins")
+print("number of data :",len(Y_test_foreign))
+print("number of features :",len(X_test_foreign[0]))
 
